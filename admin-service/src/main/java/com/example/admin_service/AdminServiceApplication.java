e2ac13d946bea2cf255eef36898420a17d2f2488
parasbramhankar <parasbramhankar.in@gmail.com>
2026-03-10 19:20:43 +0530

    Updating AdminServiceApplication

--- admin-service/src/main/java/com/example/admin_service/AdminServiceApplication.java
+++ admin-service/src/main/java/com/example/admin_service/AdminServiceApplication.java
@@ -1,9 +1,11 @@
 package com.example.admin_service;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAdminServer
 public class AdminServiceApplication {
 
 	public static void main(String[] args) {
