6ccb06fb8e0e3ff07fc7a293176ba822256391f3
parasbramhankar <parasbramhankar.in@gmail.com>
2026-03-14 23:24:49 +0530

    commit

--- auth-service/src/main/java/com/example/auth_service/security/JwtService.java
+++ auth-service/src/main/java/com/example/auth_service/security/JwtService.java
@@ -4,6 +4,7 @@ package com.example.auth_service.security;
 import com.example.auth_service.entity.User;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
@@ -12,24 +13,32 @@ import java.util.Date;
 @Service
 public class JwtService {
 
-    private static final String SECRET_KEY="parasbramhankarinojri1234456abcdefghijklmnopqrstuvwxyz";
-    private final SecretKey key= Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+    @Value("${jwt.secret")
+    private String secrete;
+
+    @Value("${jwt.expiration}")
+    private Long expiration;
+
+    private SecretKey getKey(){
+        return Keys.hmacShaKeyFor(secrete.getBytes());
+    }
 
     public String generateToken(User user){
         return Jwts.builder()
                 .subject(user.getUsername())
                 .claim("id",user.getId())
                 .claim("username",user.getUsername())
+                .claim("role",user.getRole())
                 .issuedAt(new Date())
-                .expiration(new Date(System.currentTimeMillis()+1000*60*30))
-                .signWith(key)
+                .expiration(new Date(System.currentTimeMillis()+expiration))
+                .signWith(getKey())
                 .compact();
     }
 
     public boolean isValid(String token){
         try {
             Jwts.parser()
-                    .verifyWith(key)
+                    .verifyWith(getKey())
                     .build()
                     .parseSignedClaims(token);
 
@@ -41,7 +50,7 @@ public class JwtService {
 
     public String extractUsername(String token) {
         return Jwts.parser()
-                .verifyWith(key)
+                .verifyWith(getKey())
                 .build()
                 .parseSignedClaims(token)
                 .getPayload()
