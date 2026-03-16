0da3961c0f1336df13b374f5720f2ff307e2cda1
parasbramhankar <parasbramhankar.in@gmail.com>
2026-03-16 12:35:43 +0530

    commit

---
+++ user-service/src/main/java/com/example/user_service/exception/EmailNotFoundException.java
@@ -0,0 +1,12 @@
+package com.example.user_service.exception;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class EmailNotFoundException extends RuntimeException {
+    public EmailNotFoundException(String message) {
+        super(message);
+    }
+}
