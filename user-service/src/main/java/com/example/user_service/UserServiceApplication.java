abc8b6058d9f076d494ee90ac00f54eb35678643
parasbramhankar <parasbramhankar.in@gmail.com>
2026-03-16 12:36:11 +0530

    commit

--- user-service/src/main/java/com/example/user_service/UserServiceApplication.java
+++ user-service/src/main/java/com/example/user_service/UserServiceApplication.java
@@ -1,9 +1,26 @@
 package com.example.user_service;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+
+@OpenAPIDefinition(
+		info = @Info(
+				title = "User Service API",
+				version = "v1.0",
+				description = "User Service manages user profiles, user information, and user-related operations for the Quiz Platform.",
+				contact = @Contact(
+						name = "Quiz Platform Development Team",
+						email = "support@quizplatform.com"
+				)
+		)
+)
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
