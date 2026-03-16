8731f2c487ade7bde2691126a8267b202bdeef29
parasbramhankar <parasbramhankar.in@gmail.com>
2026-03-16 12:36:01 +0530

    commit

--- user-service/src/main/java/com/example/user_service/repo/UserRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.user_service.repo;
-
-import com.example.user_service.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<User,Integer> {
-}
