7cae0916f76db261cd4c3f1d8a4d6579596fbd2b
parasbramhankar <parasbramhankar.in@gmail.com>
2026-03-19 20:28:02 +0530

    commit

---
+++ auth-service/src/main/java/com/example/auth_service/dto/UserUpdateRequestDto.java
@@ -0,0 +1,17 @@
+package com.example.auth_service.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserUpdateRequestDto {
+    private String name;
+    private String username;
+    private String email;
+}
