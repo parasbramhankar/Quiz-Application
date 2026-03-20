adecf13cd3ac535511b7b5ec383cd4513d206494
parasbramhankar <parasbramhankar.in@gmail.com>
2026-03-20 12:58:05 +0530

    quiz-service commit

---
+++ quiz-service/src/main/java/com/example/quiz_service/entity/Question.java
@@ -0,0 +1,35 @@
+package com.example.quiz_service.entity;
+
+import com.example.quiz_service.entity.enums.Difficulty;
+import com.example.quiz_service.entity.enums.Status;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "question")
+public class Question {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String questionText;
+    private String topic;
+
+    @Enumerated(EnumType.STRING)
+    private Difficulty difficulty;
+
+    private String explanation;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.ALL,orphanRemoval = true)
+    private List<Option> options;
+}
