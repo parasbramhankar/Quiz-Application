a46a71ae00f58758cf07a0b6d5d1c025c171b64b
parasbramhankar <parasbramhankar.in@gmail.com>
2026-03-22 19:11:41 +0530

    quiz-service commit

---
+++ quiz-service/src/main/java/com/example/quiz_service/dto/QuizSubmitRequest.java
@@ -0,0 +1,17 @@
+package com.example.quiz_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class QuizSubmitRequest {
+
+    private List<Integer>questionId;
+    private List<SubmitAnswerRequest>answers;
+}
