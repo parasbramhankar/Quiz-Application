8ccc42415328133d7b1a9cf7e0fa027e584de42c
parasbramhankar <parasbramhankar.in@gmail.com>
2026-03-22 19:11:51 +0530

    quiz-service commit

--- quiz-service/src/main/java/com/example/quiz_service/config/SecurityConfig.java
+++ quiz-service/src/main/java/com/example/quiz_service/config/SecurityConfig.java
@@ -1,4 +1,25 @@
 package com.example.quiz_service.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http){
+        http.csrf(c->c.disable());
+
+        http.authorizeHttpRequests(req->req
+                .requestMatchers("/admin/**").hasRole("ADMIN")
+                .requestMatchers("/quizzes/**").hasRole("USER")
+                .anyRequest().authenticated()
+        );
+        return http.build();
+    }
 }
+
